0dd620f5b6872cc586d075e45fc2591612d13ff8
gowtham thangavelu <saigowthamt@gmail.com>
2023-07-04 00:02:51 -0500

    JPChase app

--- app/src/test/java/com/login/jpmorganwheatherapp/JPLocationResponseTest.kt
+++ app/src/test/java/com/login/jpmorganwheatherapp/JPLocationResponseTest.kt
@@ -1,11 +1,10 @@
 package com.login.jpmorganwheatherapp
 
-import android.app.Activity
-import com.login.jpmorganwheatherapp.weatherapp.data.CityLocationResponse
 import com.login.jpmorganwheatherapp.weatherapp.data.Main.Companion.convertToFahrenheit
 import com.login.jpmorganwheatherapp.weatherapp.data.Sys.Companion.convertTime
 import org.junit.Assert
 import org.junit.Test
+import org.junit.Assert.*
 
 class JPLocationResponseTest {
     @Test
@@ -14,9 +13,27 @@ class JPLocationResponseTest {
         Assert.assertEquals("71.7", fahrenheit)
     }
 
+
+    @Test(expected = ArithmeticException::class)
+    fun testNotValidKelvin() {
+        val fahrenheit: Double? = convertToFahrenheit(0.0).toDoubleOrNull()
+        if (fahrenheit != null) {
+            assertEquals("Check Null", 0.0, fahrenheit, 0.0)
+        }
+    }
+
+
     @Test
     fun testConvertSunriseUnixToTime() {
         val time: String? = convertTime(1688388721)
         Assert.assertEquals("07:52:01 AM", time)
     }
+    @Test(expected = ArithmeticException::class)
+    fun testNotUnixToTime() {
+        val fahrenheit: Double? = convertTime(10000000).toDoubleOrNull()
+        if (fahrenheit != null) {
+            assertEquals("Check Null", 0.0, fahrenheit, 0.0)
+        }
+    }
+
 }
